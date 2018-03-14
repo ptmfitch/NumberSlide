9c767ddfcb230199835a41c95c2fb54267a653e1
peterf <peter.fitch@imanage.com>
2018-03-14 14:44:04 +0000

    fixed sdk and library issues

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "NumberSlide"
+
+version := "0.1"
+
+scalaVersion := "2.12.4"
--- src/main/scala/NumberSlide.scala
+++ src/main/scala/NumberSlide.scala
@@ -1,11 +1,9 @@
 import processing.core.PApplet
 
 object NumberSlide extends PApplet {
-
   def main(args: Array[String]): Unit = {
-    val processingArgs: Array[String] = Array("NumberSlide")
     val numberSlide: NumberSlide = new NumberSlide
-    PApplet.runSketch(processingArgs, numberSlide)
+    PApplet.runSketch(Array("NumberSlide"), numberSlide)
   }
 }
 
